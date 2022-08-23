4fb6b302633f9851c99083a0c77ae94a1575cae2
dakshvyas <vyas65655@gmail.com>
2022-08-22 18:24:59 -0700

    Day-1

---
+++ 1st  program.cpp
@@ -0,0 +1,14 @@
+// ******************100 Days of Code **********************//
+//*************************DAY-1*****************************// 
+
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+    char name[10];
+	cout<<"Daksh vyas\n";
+	cin>>name>>num;
+	cout<<name;
+	cout<<num;
+return 0;
+}
