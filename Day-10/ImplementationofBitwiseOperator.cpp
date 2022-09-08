87791fe74c6888135950d4be9bb3cbee5f860904
dakshvyas <vyas65655@gmail.com>
2022-09-08 07:12:22 +0530

    Day-10

---
+++ Day-10/ImplementationofBitwiseOperator.cpp
@@ -0,0 +1,33 @@
+//Implementation of Bitwise Operator
+#include<iostream>
+using namespace std;
+int main(){
+int a=4,b=6;
+
+
+cout<<(a&b)<<endl;
+cout<<(a|b)<<endl;
+cout<<(a^b)<<endl;
+cout<<(~a)<<endl;
+cout<<~b;
+
+return 0;
+}
+
+
+//Fibonacci Series
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0,b=1;
+    int n=10,next;
+    cout<<a<<" "<<b<<" ";
+   for (int i=1;i<=8;i++)
+{
+next=a+b;
+cout<<next<<" ";  
+a=b;
+b=next;
+}
+    return 0;
+}
