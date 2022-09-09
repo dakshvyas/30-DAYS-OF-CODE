4dd1c5623e860bd5e55cb01c75838a18ccbfd478
dakshvyas <vyas65655@gmail.com>
2022-09-09 06:32:53 +0530

    Day-12

---
+++ Day-12/tellgandtellpfunctions.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    ifstream fin;
+    char ch;
+    fin.open("Daksh.txt");
+    int pos;
+    pos=fin.tellg();
+    cout<<pos;
+    fin>>ch;
+    pos=fin.tellg();
+    cout<<pos;
+    return 0;
+}
+
+
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    ofstream fout;
+    char ch;
+    fout.open("Daksh.txt",ios::app);
+    int pos ;
+    pos=fout.tellp();
+    cout<<pos;
+    fout<<"Daksh Vyas";
+    fout.close();
+    return 0;
+}
+
